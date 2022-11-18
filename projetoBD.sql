1f37f1b56b67068981af95b2f0927286af4bbad0
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-11-17 21:55:11 -0300

    Finished

--- projetoBD.sql
+++ projetoBD.sql
@@ -16,9 +16,6 @@ create or replace function FN_CRIPTOGRAFIA(senha varchar2, acrescimo number)
 return varchar2
 as
 result varchar2(150) := '';
-linha1 varchar2(150) := '';
-linha2 varchar2(150) := '';
-linha3 varchar2(150) := '';
 aux number := 1;
 
 TYPE RecType IS RECORD
@@ -27,47 +24,21 @@ TYPE RecType IS RECORD
 );
 TYPE TblType IS TABLE OF RecType INDEX BY PLS_INTEGER;
 TYPE TblOfTblType IS TABLE OF TblType INDEX BY PLS_INTEGER;
-matrix   TblOfTblType;
-
+matrix TblOfTblType;
 begin
-
-    FOR i IN 1 .. 3 LOOP
-        DBMS_OUTPUT.PUT_LINE(aux);
-        FOR j IN 1 .. (length(senha) / 3) + mod(length(senha),3) LOOP
-          --matrix(i)(j).value1 := substr(senha, aux, 1);
-          if mod(j,3) = 1 then
-            matrix(1)(j).value1 := substr(senha, aux, 1);
-        elsif mod(j,3) = 2 then
-            matrix(2)(j).value1 := substr(senha, aux, 1);
-        elsif mod(j,3) = 0 then
-            matrix(3)(j).value1 := substr(senha, aux, 1);
+    for i in 0 .. length(senha) - 1 loop
+        matrix(mod(i, 3) + 1)(aux).value1 := substr(senha, i + 1, 1);
+        if mod(i, 3) = 2 then
+            aux := aux + 1;
         end if;
-          DBMS_OUTPUT.PUT('[j: ' || j || ' ' || 'i: ' || i || ']');
-          aux := aux + 1;
-        END LOOP;
-        DBMS_OUTPUT.NEW_LINE;
-    END LOOP;
+    end loop;
 
-    DBMS_OUTPUT.PUT_LINE(aux);
-
-    FOR i IN 1 .. matrix.COUNT LOOP
-        FOR j IN 1 .. matrix(i).COUNT LOOP
-          DBMS_OUTPUT.PUT( '[' || matrix(i)(j).value1|| ']');
-        END LOOP;
-        DBMS_OUTPUT.NEW_LINE;
-    END LOOP;
-
-    for i in 1 .. (LENGTH(senha)) LOOP
-        if mod(i,3) = 1 then
-            linha1 := linha1 || '0' || to_char(to_number(ascii(substr(senha, i, 1)), '99999.999') + acrescimo);
-        elsif mod(i,3) = 2 then
-            linha2 := linha2 || '0' || to_char(to_number(ascii(substr(senha, i, 1)), '99999.999') + acrescimo);
-        elsif mod(i,3) = 0 then
-            linha3 := linha3 || '0' || to_char(to_number(ascii(substr(senha, i, 1)), '99999.999') + acrescimo);
-        end if;
-    END LOOP;
-    result := linha1||linha2||linha3;
-return result;
+    for x in 1 .. 3 loop
+        for y in 1 .. matrix(x).COUNT loop
+            result := result || '0' || to_char(to_number(ascii(matrix(x)(y).value1), '99999.999') + acrescimo);
+        end loop;
+    end loop;
+    return result;
 end;
 /
 
@@ -78,11 +49,7 @@ create or replace function FN_DESCRIPTOGRAFIA(senha varchar2, acrescimo number)
 return varchar2
 as
 result varchar2(150) := '';
-linha1 varchar2(150) := '';
-linha2 varchar2(150) := '';
-linha3 varchar2(150) := '';
 strAuxiliar varchar2(150) := '';
-tamanho number := length(senha) / 3;
 aux number := 1;
 
     TYPE RecType IS RECORD
@@ -92,40 +59,31 @@ aux number := 1;
   TYPE TblType IS TABLE OF RecType INDEX BY PLS_INTEGER;
   TYPE TblOfTblType IS TABLE OF TblType INDEX BY PLS_INTEGER;
   matrix   TblOfTblType;
+    tamanho number := length(senha) / 3;
+    aux2 number := 1;
+    aux3 number := 0;
 begin
-
-    FOR i IN 1 .. 3 LOOP
-    FOR j IN 1 .. 4 LOOP
-      matrix(i)(j).value1 := i;
-    END LOOP;
-  END LOOP;
-
-    FOR i IN 1 .. matrix.COUNT LOOP
-        FOR j IN 1 .. matrix(i).COUNT LOOP
-          DBMS_OUTPUT.PUT( '[' || matrix(i)(j).value1|| ']' || CHR(11) );
-        END LOOP;
-        DBMS_OUTPUT.NEW_LINE;
-    END LOOP;
 
     for i in 1 .. tamanho LOOP
         strAuxiliar := strAuxiliar || chr('0'||to_char(to_number(substr(senha, aux, 3), '99999.999') - acrescimo));
-        --linha3 := linha3 || chr('0'||to_char(to_number(substr(senha, aux, 3), '99999.999') - acrescimo));
-        --DBMS_OUTPUT.PUT_LINE(chr('0'||to_char(to_number(substr(senha, aux, 3), '99999.999') - acrescimo)));
         aux := aux + 3;
     END LOOP;
-    DBMS_OUTPUT.PUT_LINE(strAuxiliar);
-
-    for i in 1 .. length(strAuxiliar) loop
-        DBMS_OUTPUT.PUT_LINE(i);
-        if mod(i,3) = 1 then
-            linha1 := linha1 || substr(strAuxiliar, i, 1);
-        elsif mod(i,3) = 2 then
-            linha2 := linha2 || substr(strAuxiliar, i, 1);
-        elsif mod(i,3) = 0 then
-            linha3 := linha3 || substr(strAuxiliar, i, 1);
-        end if;
+    aux := mod(tamanho, 3);
+    for i in 1 .. (tamanho / 3) + 1 loop
+        aux2 := i;
+        aux3 := aux;
+        for x in 1 .. 3 loop
+            if length(result) = length(strAuxiliar) then
+                exit;
+            end if;
+            result := result||substr(strAuxiliar, aux2, 1);
+            if aux3 > 0 then
+                aux2 := aux2 + 1;
+                aux3 := aux3 - 1;
+            end if;
+            aux2 := aux2 + 3;
+        end loop;
     end loop;
-    result := linha1 || linha2 || linha3;
     return result;
 end;
 /
@@ -157,3 +115,34 @@ drop table acesso;
 select * from login join acesso on login.cod_login = acesso.cod_login;
 insert into login values(1, 'a', '1');
 call PR_INSERE_LOGIN(2, 'b', 'COTEMIG123');
+
+create or replace procedure PR_VALIDA_LOGIN(login varchar2, senha varchar2)
+as
+    cursor cr_login is select cod_login, login, senha from login;
+    dt timestamp := SYSTIMESTAMP;
+    pass varchar2(150) := '';
+    pwd varchar(150) := '';
+    aux boolean := true;
+begin
+    for x in cr_login loop
+        if x.login = login then
+            select data_hora into dt from acesso where cod_login = x.cod_login;
+            select FN_DESCRIPTOGRAFIA(x.senha, to_number(to_char(dt, 'FF1'))) into pass from dual;
+            if pass = senha then
+                select FN_CRIPTOGRAFIA(senha, to_number(to_char(dt, 'FF1'))) into pwd from dual;
+                update login set senha = pwd where cod_login = x.cod_login;
+                update acesso set data_hora = dt where cod_login = x.cod_login;
+                DBMS_OUTPUT.PUT_LINE('Login efetuado com sucesso!');
+                aux := false;
+            else
+                raise_application_error(-20003, 'Senha incorreta!');
+            end if;
+        end if;
+    end loop;
+    if aux then
+        raise_application_error(-20002, 'Login não encontrado!');
+    end if;
+end;
+/
+
+call PR_VALIDA_LOGIN('b', 'COTEMIG123');
